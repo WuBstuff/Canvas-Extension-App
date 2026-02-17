5d61120ffe1eaf2d16b613f09159db992af116d0
Arnav Patel <arn4v.patel@csu.fullerton.edu>
2026-02-16 19:12:47 -0800

    Update proto_dashboard.py

--- proto_dashboard.py
+++ proto_dashboard.py
@@ -1,57 +1,40 @@
-#This is a barebones prototype of how we want the Canvas dashboard to look like using StreamLit
+#This is a barebones prototype of how we want the Canvas dashboard to look like through StreamLit
 #Some stuff to work on here would be the formatting of the dashboard as well as adding some extra things but this is just the basics
 #In order to run this:
-# type "python -m venv venv" into the terminal, this will create the venv folder needed to run the venv VM, don't need to do this if you already have a venv folder
-# type "venv/bin/activate" into the terminal, this activates the Venv VM
+# type "python -m venv venv" into the terminal, this will create the venv folder needed to run the venv VM
+# type "source venv/bin/activate" into the terminal, this activates the Venv VM
 # ensure that the debugger on VSCode or whatever IDE is set to the Venv version
 # run the file with "streamlit run proto_dashboard.py", this opens up a window on your browser with the dashboard
 
 import streamlit as sl
+import assignment #implement later, currently working on events
 
 #Page Settings
 sl.set_page_config(
-    page_title = "Canvas Dashboard",
+    page_title = "My Canvas Dashboard",
     layout = "wide"
 )
 sl.title("Welcome to your Canvas Dashboard!")
-sl.markdown("Here is the plan")
+
+#Sample Values
+assignments = ("Assignment 1", "Assignment 2", "Quiz 1")
+due_dates = ("Tomorrow 11:59pm", "Thursday 8am", "Next Monday from 8am to 10am")
+advices = ("Get it done ASAP, today afterschool would be good", "You got some time, maybe put it off to work on Assignment 1", "Shouldn't be too bad; an hour or two of studying should be fine")
 
 #Workload Display
 col1, col2, col3 = sl.columns(3)
 with col1:
-    sl.subheader("To-Do List:")
+    sl.subheader("Here is the (sample) plan:")
 with col2:
     sl.button("Refresh Token")
 with col3:
     sl.button("Input Token")
 
-col1, col2, col3 = sl.columns(3)
-with col1:
-    sl.caption("Assignment 1")
-with col2:
-    sl.caption("Due by Tomorrow 11:59pm")
-with col3:
-    sl.caption("Get it done ASAP, today afterschool would be good")
-
-col1, col2, col3 = sl.columns(3)
-with col1:
-    sl.caption("Assignment 2")
-with col2:
-    sl.caption("Due in 3 days 8am")
-with col3:
-    sl.caption("You got some time, maybe put it off to work on Assignment 1")
-
-col1, col2, col3 = sl.columns(3)
-with col1:
-    sl.caption("Quiz 1")
-with col2:
-    sl.caption("Starts next Monday from 8am to 10am")
-with col3:
-    sl.caption("Shouldn't be too bad; an hour or two of studying should be fine")
-
-#Edit Dashboard
-col1, col2 = sl.columns(2)
-with col1:
-    sl.button("Add Assignment")
-with col2:
-    sl.button("Edit Assignment")
+for index in range(len(assignments)):
+    col1, col2, col3 = sl.columns(3)
+    with col1:
+        sl.caption(assignments[index])
+    with col2:
+        sl.caption(due_dates[index])
+    with col3:
+        sl.caption(advices[index])
